20713ad494f54a35f0bb470151ffeab543bdbf97
Chris Mims <chritopherm970@aol.com>
2020-09-28 21:38:47 -0500

    Updates

---
+++ precache-manifest.cb8f0453b591decd5a5c049b2509bca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f236335ec964fdda0a1167365a48aec2",
+    "url": "/Portfolio-Update/index.html"
+  },
+  {
+    "revision": "db291990c16d6edac45d",
+    "url": "/Portfolio-Update/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e62ee39b92208ff2b191",
+    "url": "/Portfolio-Update/static/css/main.4b5f6231.chunk.css"
+  },
+  {
+    "revision": "db291990c16d6edac45d",
+    "url": "/Portfolio-Update/static/js/2.ddad7584.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-Update/static/js/2.ddad7584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62ee39b92208ff2b191",
+    "url": "/Portfolio-Update/static/js/main.302c42be.chunk.js"
+  },
+  {
+    "revision": "d5f6f44424720c28bb22",
+    "url": "/Portfolio-Update/static/js/runtime-main.23068812.js"
+  }
+]);
